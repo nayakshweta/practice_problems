064f1e612a5b77578feb07f6836cecba8b336349
Shweta <nayakshweta07@gmail.com>
2019-07-28 13:46:35 +0530

    Decorator vs custom decorator.

--- decorator_smart_divide.py
+++ decorator_smart_divide.py
@@ -1,21 +1,38 @@
-import sys
 
-def smart_divide(divide):
+def smart_divide(func):
     def inner(x, y):
-        print "I am going to divide", x, "and", y
+        print "Dividing", x, "and", y
         if y == 0:
-            print "This is operation is invalid!"
+            print "Cant divide by zero"
             return
         else:
-            return divide(x, y)
+            return func(x, y)
     return inner
 
+
+def get_custom_decorator(msg1, msg2):
+    def smart_divide(func):
+        def inner(x, y):
+            print msg1, x, "and", y
+            if y == 0:
+                print msg2
+                return
+            else:
+                return func(x, y)
+        return inner
+    return smart_divide
+
 @smart_divide
-def divide(x, y):
+def divide1(x, y):
+    result = x / y
+    print result
+
+@get_custom_decorator("Dividing", "Can't divide by zero.")
+def divide2(x, y):
     result = x / y
     print result
 
 if __name__ == "__main__":
     x = int(raw_input("Enter first number for division: "))
     y = int(raw_input("Enter second number for division: "))
-    divide(x, y)
+    divide2(x, y)
